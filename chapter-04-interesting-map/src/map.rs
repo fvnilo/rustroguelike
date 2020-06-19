9611b74592d5eda134009062f25a4ac1d716a706
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2020-06-18 22:25:20 -0400

    Implement Chapter 4: An interesting map
    Placing the bases for the roguelike games:
    - Separate modules
    - Randomly generated rooms
    - Link room with tunnels

--- chapter-04-interesting-map/src/map.rs
+++ chapter-04-interesting-map/src/map.rs
@@ -1,4 +1,6 @@
-use rltk::{Rltk, RGB};
+use super::Rect;
+use rltk::{RandomNumberGenerator, Rltk, RGB};
+use std::cmp::{max, min};
 
 // The various type of tiles you can have on a map.
 #[derive(PartialEq, Copy, Clone)]
@@ -11,7 +13,9 @@ pub fn xy_idx(x: i32, y: i32) -> usize {
     (y as usize * 80) + x as usize
 }
 
-pub fn new_map() -> Vec<TileType> {
+/// Makes a map with solid boundaries and 400 randomly placed walls.
+/// No guarantees that it won't look awful.
+pub fn new_map_test() -> Vec<TileType> {
     let mut map = vec![TileType::Floor; 80 * 50];
 
     // Make the boundaries walls
@@ -38,6 +42,99 @@ pub fn new_map() -> Vec<TileType> {
     }
 
     map
+}
+
+/// Generate a random set of rooms and place on a 80x50 map. All of the
+/// rooms are then linked together with corridors.
+pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
+    let mut map = vec![TileType::Wall; 80 * 50];
+
+    let mut rooms: Vec<Rect> = Vec::new();
+    // There can not be more than 30 rooms per map
+    const MAX_ROOMS: i32 = 30;
+    // Maximum sizes of a room
+    const MIN_SIZE: i32 = 6;
+    const MAX_SIZE: i32 = 10;
+
+    let mut rng = RandomNumberGenerator::new();
+
+    for _ in 0..MAX_ROOMS {
+        // Create the room randomly
+        let w = rng.range(MIN_SIZE, MAX_SIZE);
+        let h = rng.range(MIN_SIZE, MAX_SIZE);
+
+        // Randomly assign coordinates to the room
+        // Notice that the arguments to roll_dice are
+        // between 1 and X -1 so we maintain a wall
+        // around the map
+        let x = rng.roll_dice(1, 80 - w - 1) - 1;
+        let y = rng.roll_dice(1, 50 - h - 1) - 1;
+        let new_room = Rect::new(x, y, w, h);
+
+        let mut ok = true;
+        // Reject the room if it overlaps an already created room
+        for other_room in rooms.iter() {
+            if new_room.intersect(other_room) {
+                ok = false
+            }
+        }
+        if ok {
+            apply_room_to_map(&new_room, &mut map);
+
+            if !rooms.is_empty() {
+                let (new_x, new_y) = new_room.center();
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+                // Apply a tunnel between the two rooms.
+                // We choose the order or the tunnel application
+                // based on a 50/50 random value.
+                //
+                // The way these tunnel works is that it will create "L's"
+                // by linking the center of both rooms U/D then L/R or
+                // the other way around.
+                if rng.range(0, 2) == 1 {
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                } else {
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                }
+            }
+
+            rooms.push(new_room);
+        }
+    }
+
+    (rooms, map)
+}
+
+/// Create an horizontal tunnel between two rooms.
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+    for x in min(x1, x2)..=max(x1, x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+/// Create a vertical tunnel between two rooms.
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+    for y in min(y1, y2)..=max(y1, y2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+/// Draw a room on a map.
+fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
+    for y in room.y1 + 1..=room.y2 {
+        for x in room.x1 + 1..=room.x2 {
+            map[xy_idx(x, y)] = TileType::Floor;
+        }
+    }
 }
 
 pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
--- chapter-04-interesting-map/src/player.rs
+++ chapter-04-interesting-map/src/player.rs
@@ -18,13 +18,22 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
 }
 
 pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
     match ctx.key {
         None => {} // Nothing happened
         Some(key) => match key {
             VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Numpad4 => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
             VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Numpad6 => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad8 => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad2 => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
         },
     }
